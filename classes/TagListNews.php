32e6d61ac01f94bc0cfe5c1ca6e9c318df68df95
Helmut Schottmüller <helmut.schottmueller@me.com>
2012-11-07 09:34:25 +0100

    Fixed namespace problem

--- classes/TagListNews.php
+++ classes/TagListNews.php
@@ -10,7 +10,7 @@ namespace Contao;
  * @author     Helmut Schottmüller <typolight@aurealis.de>
  * @package    Controller
  */
-class TagListNews extends \TagList
+class TagListNews extends TagList
 {
 	protected $arrNewsarchives = array();
 	
